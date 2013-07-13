dd15cf93c2721c98d40d85a4d643be904d68b8fc
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-13 18:06:24 +0200

    Further WPF boilerplate added

---
+++ KinectICP/ViewModels/DelegatingCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace KinectICP.ViewModels
+{
+    class DelegatingCommand: ICommand
+    {
+        private Action<object> _action;
+        
+        public event EventHandler CanExecuteChanged;
+
+        public DelegatingCommand(Action<object> action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+    }
+}
--- KinectICP/ViewModels/MainViewModel.cs
+++ KinectICP/ViewModels/MainViewModel.cs
@@ -1,29 +1,39 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace KinectICP.ViewModels
 {
     class MainViewModel: ViewModel
     {
-        public bool IsConnected
+        private WriteableBitmap _bitmap;
+
+        public MainViewModel()
         {
-            get
-            {
-                //var sensor = KinectSensor.KinectSensors.First();
-                //var depthStream = sensor.DepthStream;
-                //depthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-                //var frame = depthStream.OpenNextFrame(1000 / 30);
-                //depthStream.Disable();
-                return false;
-            }
+            _bitmap = new WriteableBitmap(320, 200, 96, 96, PixelFormats.Gray16, null);
         }
 
-        public void Refresh()
+        public WriteableBitmap DepthMap
         {
-            RaisePropertyChanged("IsConnected");
+            get { return _bitmap; }
         }
+
+        public ICommand Start { get { return new DelegatingCommand(StartSensor); } }
+
+        public void StartSensor(object parameter)
+        {
+            var sensor = KinectSensor.KinectSensors.First();
+            var depthStream = sensor.DepthStream;
+            depthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            var frame = depthStream.OpenNextFrame(1000 / 30);
+            depthStream.Disable();
+        }
+
     }
 }
