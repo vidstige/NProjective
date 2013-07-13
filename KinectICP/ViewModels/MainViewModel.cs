24240c8650a03d131f45ecce9dc5ec3ada1e4593
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-13 20:12:49 +0200

    Displaying crude depth map

--- KinectICP/ViewModels/MainViewModel.cs
+++ KinectICP/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -12,11 +13,12 @@ namespace KinectICP.ViewModels
 {
     class MainViewModel: ViewModel
     {
+        private KinectSensor _sensor;
         private WriteableBitmap _bitmap;
 
         public MainViewModel()
         {
-            _bitmap = new WriteableBitmap(320, 200, 96, 96, PixelFormats.Gray16, null);
+            _bitmap = new WriteableBitmap(640, 480, 96, 96, PixelFormats.Gray8, null);
         }
 
         public WriteableBitmap DepthMap
@@ -28,12 +30,27 @@ namespace KinectICP.ViewModels
 
         public void StartSensor(object parameter)
         {
-            var sensor = KinectSensor.KinectSensors.First();
-            var depthStream = sensor.DepthStream;
+            _sensor = KinectSensor.KinectSensors.First();
+            var depthStream = _sensor.DepthStream;
             depthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
-            var frame = depthStream.OpenNextFrame(1000 / 30);
-            depthStream.Disable();
+            _sensor.DepthFrameReady += _sensor_DepthFrameReady;
+            _sensor.Start();
         }
 
+        private void _sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenDepthImageFrame())
+            {
+                if (frame != null)
+                {
+                    DepthImagePixel[] pixels = new DepthImagePixel[frame.PixelDataLength];
+                    frame.CopyDepthImagePixelDataTo(pixels);
+                    var buff = pixels.Select(pixel => (byte)(256-pixel.Depth / 256)).ToArray();
+                    var rect = new Int32Rect(0, 0, 640, 480);
+                    const int stride = 640;
+                    _bitmap.WritePixels(rect, buff, stride, 0);
+                }
+            }
+        }
     }
 }
