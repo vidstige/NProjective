613e18761992bfb80d1aed4868a87904c58ea1a8
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-07-13 17:51:13 +0200

    Traditional boilerplate added

---
+++ KinectICP/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectICP.ViewModels
+{
+    class MainViewModel: ViewModel
+    {
+        public bool IsConnected
+        {
+            get
+            {
+                //var sensor = KinectSensor.KinectSensors.First();
+                //var depthStream = sensor.DepthStream;
+                //depthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                //var frame = depthStream.OpenNextFrame(1000 / 30);
+                //depthStream.Disable();
+                return false;
+            }
+        }
+
+        public void Refresh()
+        {
+            RaisePropertyChanged("IsConnected");
+        }
+    }
+}
---
+++ KinectICP/ViewModels/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectICP.ViewModels
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            var tmp = PropertyChanged;
+            if (tmp != null) tmp(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
